0389468d7a2af13e9a1fad6a79a2547b51f3913d
sunny442k3 <phucvan.ai.442k3@gmail.com>
2022-09-07 17:04:39 +0700

    Linked List

--- linked_list.py
+++ linked_list.py
@@ -1,56 +1,57 @@
 class Node:
-    def __init__(self, head, next_node=None):
-        self.head = head 
-        self.next_node = next_node 
+    def __init__(self, data, next_node=None):
+        self.data = data 
+        self.next = next_node 
 
     
 class LinkedList:
     def __init__(self):
-        self.first_node = None
+        self.head = None
 
     def add_node(self, data):
         new_node = Node(data)
-        if self.first_node is None:
-            self.first_node = new_node 
+        if self.head is None:
+            self.head = new_node 
         else:
-            current_node = self.first_node 
-            while current_node.next_node is not None:
-                current_node = current_node.next_node 
-            current_node.next_node = new_node  
+            current_node = self.head 
+            while current_node.next is not None:
+                current_node = current_node.next 
+            current_node.next = new_node  
         
     def remove_node(self, data):
-        if self.first_node is None:
-            return 
-        current_node = self.first_node
-        if current_node.head == data:
-            self.first_node = current_node.next_node
+        if self.head is None:
             return 
 
-        while current_node.next_node is not None:
-            nxt_node = current_node.next_node
-            if nxt_node.head == data:
-                current_node.next_node = nxt_node.next_node
+        current_node = self.head
+        if current_node.data == data:
+            self.head = current_node.next
+            return 
+
+        while current_node.next is not None:
+            nxt_node = current_node.next
+            if nxt_node.data == data:
+                current_node.next = nxt_node.next
                 return 
 
     def print_list(self):
-        current_node = self.first_node
+        current_node = self.head
         if current_node is None:
             return None 
-        print(current_node.head, end=" ")
-        while current_node.next_node is not None:
-            current_node = current_node.next_node
-            print(current_node.head, end = " ")
+        print(current_node.data, end=" ")
+        while current_node.next is not None:
+            current_node = current_node.next
+            print(current_node.data, end = " ")
         print()
 
 
     def get_len(self):
-        if self.first_node is None:
+        if self.head is None:
             return 0
         len_list  = 1
-        current_node = self.first_node
-        while current_node.next_node is not None:
+        current_node = self.head
+        while current_node.next is not None:
             len_list += 1
-            current_node = current_node.next_node 
+            current_node = current_node.next
         return len_list 
 
 
